e388c96679985d26a9ab4298336ea547bebdcbf7
cryptnology <jamie@cryptnology.dev>
2023-04-29 08:23:41 +0100

    Add google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import { getServerSession } from 'next-auth';
+import { Roboto, Lobster_Two } from 'next/font/google';
 
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { Hydrate, NavBar } from '@/components';
 
 import './globals.css';
+
+const roboto = Roboto({ weight: ['400', '500', '700'], subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -20,7 +23,7 @@ const RootLayout = async ({ children }: Props) => {
 
   return (
     <html lang="en">
-      <body className="mx-64">
+      <body className={`mx-64 ${roboto.className}`}>
         <Hydrate>
           <NavBar user={session?.user} expires={session?.expires as string} />
           {children}
