76e5f352cb48af12666cef683710328949c19b55
cryptnology <jamie@cryptnology.dev>
2023-04-30 12:34:57 +0100

    Dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,71 @@
-const Dashboard = () => {
+import Image from 'next/image';
+import { formatPrice } from '@/utils';
+import { PrismaClient } from '@prisma/client';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+
+export const revalidate = 0;
+
+const fetchOrders = async () => {
+  const prisma = new PrismaClient();
+  const user = await getServerSession(authOptions);
+  if (!user) return null;
+
+  return await prisma.order.findMany({
+    where: { userId: user?.user?.id },
+    include: {
+      products: true,
+    },
+  });
+};
+
+const Dashboard = async () => {
+  const orders = await fetchOrders();
+
+  if (orders === null)
+    return <div>You need to be logged in to view your orders.</div>;
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1 className="font-bold">
+        {orders.length === 0 ? 'No Orders' : 'Your Orders'}
+      </h1>
+      <div className="font-medium">
+        {orders?.map((order) => (
+          <div key={order.id} className="rounded-lg p-8 my-12">
+            <h2>Order reference: {order.id}</h2>
+            <p>Time: {new Date(order.createdDate).toString()}</p>
+            <p className="text-md py-2">
+              Status:{' '}
+              <span
+                className={`${
+                  order.status === 'complete' ? 'bg-teal-500' : 'bg-orange-500'
+                } capitalize text-white py-1 rounded-md px-2 mx-2 text-sm`}
+              >
+                {order.status}
+              </span>
+            </p>
+            <p className="font-medium">Total: {formatPrice(order.amount)}</p>
+            <div className="flex gap-8">
+              {order.products.map((product) => (
+                <div key={product.id} className="py-2">
+                  <h2 className="py-2">{product.name}</h2>
+                  <div className="flex items-center gap-4">
+                    <Image
+                      src={product.image!}
+                      alt={product.name}
+                      height={36}
+                      width={36}
+                    />
+                    <p>{formatPrice(product.unit_amount)}</p>
+                    <p>Quantity: {product.quantity}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
